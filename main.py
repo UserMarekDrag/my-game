f7584f0c215055d523cfeb977356995c542e3b44
Marek <marekdrag@gmail.com>
2023-01-14 13:49:45 +0100

    Added enemy and fight system as well as background

--- main.py
+++ main.py
@@ -6,37 +6,95 @@ WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Game!")
 
 WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+YELLOW = (255, 255, 0)
+
 FPS = 60
 PLAYER_WIDTH, PLAYER_HEIGHT = 80, 160
-VEL = 5
-BULLET_VEL = 7
+VEL_PLAYER = 5
+VEL_ENEMY = 2
+BULLET_VEL = 5
+MAX_BULLETS = 3
+
+PLAYER_HIT = pygame.USEREVENT + 1
+ENEMY_HIT = pygame.USEREVENT + 2
 
 PLAYER_IMAGE = pygame.image.load(
     os.path.join('Assets', 'player.png'))
 PLAYER = pygame.transform.scale(PLAYER_IMAGE, (PLAYER_WIDTH, PLAYER_HEIGHT))
+ENEMY_IMAGE = pygame.image.load(
+    os.path.join('Assets', 'enemy.png'))
+ENEMY = pygame.transform.scale(ENEMY_IMAGE, (PLAYER_WIDTH, PLAYER_HEIGHT))
+
+BACKGROUND = pygame.transform.scale(pygame.image.load(
+    os.path.join('Assets', 'background.png')), (WIDTH, HEIGHT))
 
 
-def draw_windows(player):
-    WIN.fill(WHITE)
+def draw_windows(player, enemy, enemy_bullets, player_bullets):
+    WIN.blit(BACKGROUND, (0, 0))
     WIN.blit(PLAYER, (player.x, player.y))
+    WIN.blit(ENEMY, (enemy.x, enemy.y))
+
+    for bullet in enemy_bullets:
+        pygame.draw.rect(WIN, RED, bullet)
+
+    for bullet in player_bullets:
+        pygame.draw.rect(WIN, YELLOW, bullet)
+
     pygame.display.update()
 
 
 def player_handle_movement(keys_pressed, player):
-    if keys_pressed[pygame.K_LEFT] and player.x - VEL > 0:  # LEFT
-        player.x -= VEL
-    if keys_pressed[pygame.K_RIGHT] and player.x + VEL + player.width < WIDTH:  # RIGHT
-        player.x += VEL
-    if keys_pressed[pygame.K_UP] and player.y - VEL > 0:  # UP
-        player.y -= VEL
-    if keys_pressed[pygame.K_DOWN] and player.y + VEL + player.height < HEIGHT:  # DOWN
-        player.y += VEL
+    if keys_pressed[pygame.K_LEFT] and player.x - VEL_PLAYER > 0:  # LEFT
+        player.x -= VEL_PLAYER
+    if keys_pressed[pygame.K_RIGHT] and player.x + VEL_PLAYER + player.width < WIDTH:  # RIGHT
+        player.x += VEL_PLAYER
+    if keys_pressed[pygame.K_UP] and player.y - VEL_PLAYER > 0:  # UP
+        player.y -= VEL_PLAYER
+    if keys_pressed[pygame.K_DOWN] and player.y + VEL_PLAYER + player.height < HEIGHT:  # DOWN
+        player.y += VEL_PLAYER
+
+
+def enemy_handle_movement(enemy, player):
+    if player.x > enemy.x:
+        enemy.x += VEL_ENEMY
+    if player.x < enemy.x:
+        enemy.x -= VEL_ENEMY
+    if player.y > enemy.y:
+        enemy.y += VEL_ENEMY
+    if player.y < enemy.y:
+        enemy.y -= VEL_ENEMY
+
+
+def handle_bullets(player_bullets, player, enemy_bullets, enemy):
+    for bullet in player_bullets:
+        bullet.x += BULLET_VEL
+        if enemy.colliderect(bullet):
+            pygame.event.post(pygame.event.Event(ENEMY_HIT))
+            player_bullets.remove(bullet)
+        elif bullet.x > WIDTH:
+            player_bullets.remove(bullet)
+        elif bullet.x < 0:
+            player_bullets.remove(bullet)
+
+    for bullet in enemy_bullets:
+        bullet.x -= BULLET_VEL
+        if player.colliderect(bullet):
+            pygame.event.post(pygame.event.Event(PLAYER_HIT))
+            enemy_bullets.remove(bullet)
+        elif bullet.x > WIDTH:
+            enemy_bullets.remove(bullet)
+        elif bullet.x < 0:
+            enemy_bullets.remove(bullet)
 
 
 def main():
-    player = pygame.Rect(300, 100, PLAYER_WIDTH, PLAYER_HEIGHT)
+    player = pygame.Rect(300, 150, PLAYER_WIDTH, PLAYER_HEIGHT)
+    enemy = pygame.Rect(700, 150, PLAYER_WIDTH, PLAYER_HEIGHT)
 
-    bullets = []
+    player_bullets = []
+    enemy_bullets = []
 
     clock = pygame.time.Clock()
     run = True
@@ -47,11 +105,24 @@ def main():
                 run = False
 
             if event.type == pygame.KEYDOWN:
-                pass
+                if event.key == pygame.K_z and len(player_bullets) < MAX_BULLETS:
+                    bullet = pygame.Rect(
+                        player.x + player.width, player.y + player.height//2 - 2, 10, 5)
+                    player_bullets.append(bullet)
+
+                if (player.x, player.y) != (enemy.x, enemy.y) and len(enemy_bullets) < MAX_BULLETS:
+                    bullet = pygame.Rect(
+                        enemy.x, enemy.y + enemy.height//2 - 2, 10, 5)
+                    enemy_bullets.append(bullet)
 
         keys_pressed = pygame.key.get_pressed()
+
+        enemy_handle_movement(enemy, player)
         player_handle_movement(keys_pressed, player)
-        draw_windows(player)
+
+        handle_bullets(player_bullets, player, enemy_bullets, enemy)
+
+        draw_windows(player, enemy, enemy_bullets, player_bullets)
 
     pygame.quit()
 
